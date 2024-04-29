253f1410a7f74b8727be9d956c1bf874307e0114
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-04-28 19:25:03 -0500

    added user model, started thought model

--- models/User.js
+++ models/User.js
@@ -2,9 +2,27 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
    username: { type: String, required: true, trim: true, unique: true},
-   email: {type: String, required: true, unique: true},
-   thoughts
-   friends
-   //need a virtual 'friendCount' as well 
+   email: {
+    type: String, 
+    required: true, 
+    unique: true, 
+    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+    },
+    //thoughts references the thought model
+   thoughts: [{ type: Schema.Types.ObjectId, ref: 'thought' }],
+   //friends self-references the user model
+   friends:[{ type: Schema.Types.ObjectId, ref: 'user' }]
+   
    //used lastAccessed to get current data: do I need this for the user?
-})
+});
+
+//'friendCount' virtual to retrieve the legnth of the user's friends array field on query 
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+
+//create a const to hold the User model
+const User = mongoose.model('User', userSchema);
+
+//export the User model for use in other directories
+module.exports = User;
